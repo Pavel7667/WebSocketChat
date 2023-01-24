65219750e9666da3625d96f3e10d3514d2d71c83
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-01-24 16:36:17 +0200

    -add ChatRoomSocketConfig

---
+++ src/main/java/com/pavelhnelitcya/WebSocketChat/chat/config/ChatRoomSocketConfig.java
@@ -0,0 +1,31 @@
+package com.pavelhnelitcya.WebSocketChat.chat.config;
+
+
+import com.pavelhnelitcya.WebSocketChat.chat.service.ChatRoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+
+import java.util.Map;
+
+/**
+ * If User call this URL, send "Bean" with this method
+ */
+@Configuration
+public class ChatRoomSocketConfig {
+
+    @Autowired
+    private ChatRoomService chatRoomService;
+
+    @Bean
+    public HandlerMapping handlerMapping(){
+        Map<String, WebSocketHandler> map = Map.of(
+                "/chat", chatRoomService
+        );
+        return new SimpleUrlHandlerMapping(map, -1);
+    }
+
+}
